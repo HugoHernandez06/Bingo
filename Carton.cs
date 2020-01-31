330797574d70f7e6a027f50139720ca21d913e95
HugoHernandez06 <44584564+HugoHernandez06@users.noreply.github.com>
2020-01-31 07:08:12 -0500

    Add files via upload

---
+++ Carton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bingo
+{
+    class Carton
+    {
+        private string[] tabla1;
+        private int[,] carton1;
+        public Carton( int[,]carton)
+        {
+            Carton1 = carton;
+        }
+
+        public string[] Tabla1 { get => tabla1; set => tabla1 = value; }
+        public int[,] Carton1 { get => carton1; set => carton1 = value; }
+
+        public string imprimirTabla()
+        {
+            string matriz = "";
+
+            for(int i =0; i<carton1.GetLength(0); i++)
+            {
+                for(int j =0; j< carton1.GetLength(0); j++)
+                {
+                    matriz += carton1[i, j] + " | ";
+                }
+                matriz += "\n";
+            }
+            Console.WriteLine("  B " + "  I " + "  N " + "  G " + "  O ");
+            return matriz ;
+        }
+        public int [,]Marcar(string ubicacion)
+        {
+            string[] split = (ubicacion.Split(','));
+            for(int i=0; i < carton1.GetLength(0); i++)
+            {
+                for (int j = 0; j < carton1.GetLength(0); j++)
+                {
+                    if (i == Convert.ToInt32(split[0]) && j == Convert.ToInt32(split[1]))
+                    {
+                        carton1[i, j] = 0;
+                    }
+                }
+            }
+            return carton1;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Bingo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 1;
+            int max = 15;
+            string marcar;
+
+            int[,] carton = new int[5, 5];
+            var aleatorio = new Random();
+
+            for (int i=0; i<carton.GetLength(0); i++)
+            {
+                for (int j = 0; j < carton.GetLength(0); j++)
+                {
+                    if (i == 2 && j == 2)
+                    {
+                        carton[i, j] = 0;
+                    }
+                    else
+                    {
+                        carton[j, i] = aleatorio.Next(min, max);
+                    }
+                }
+                min+=15;
+                max+=15;
+            }
+            Carton tablero1 = new Carton(carton);
+
+            Console.WriteLine(tablero1.imprimirTabla());
+
+            for(int i=0;i<26;i++)
+            {
+                marcar = Console.ReadLine();
+                tablero1.Marcar(marcar);
+                Console.WriteLine(tablero1.imprimirTabla());
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
